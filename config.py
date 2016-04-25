f3dee7dd1653bb4d30c2281a6ffdf31087ca27ba
Shrirang Mundada <shrirang.mundada@gmail.com>
2016-04-25 13:28:56 +0530

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-SQLALCHEMY_DATABASE_URI = 'postgres://jlwvlmhklvebfe:UvxMQp94xln2zP2uR6n0wWPS38@ec2-54-163-247-73.compute-1.amazonaws.com:5432/d27s1jj9ebn4s1'
-SECRET_KEY = "creativegffhjjgkgf"
-GOOGLE_ID = "444471952889-7iq338mtrurncu4hjc3o7qk5viu50egh.apps.googleusercontent.com"
-GOOGLE_SECRET = "jBiNRLToqk2zHsijJ4sY7Ni-"
-RAZOR_KEY = "rzp_test_wgHMJLzmklO8rQ"
-RAZOR_SECRET = "qiLr6r8ro8mjc9H0DMGmbuJg"
